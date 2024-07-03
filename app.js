70b6ebd0db9c49c47c64b213a99594ce48136c09
CrisNog <cnogales.rod@gmail.com>
2024-07-03 18:24:52 +0100

    break app

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ const PORT = process.env.PORT || 3000
 
 app.use(expressStatic('dist'))
 
+// app.get('/health', (req, res) => {
+//   res.send('ok')
+// })
+
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw 'error...  '
   res.send('ok')
 })
 
